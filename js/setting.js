38870cb35bd6b1b81da5ace2260ec38a948089b7
uchinada5374 <enviro@town.uchinada.lg.jp>
2014-07-02 13:57:01 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -2,11 +2,24 @@
  * 5374 setting
  *
 */
+/*
+const SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+const MaxDescription = 3; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
-var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+const MaxMonth = 3;
+
+const WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+*/
+
+//IE対応 const -> var
+var SVGLabel = false;
+// SVGイメージを使用するときは、true。用意できない場合はfalse。
+
+var MaxDescription = 9;
+// ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 3;
 
-var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+var WeekShift = false;
+// 休止期間なら週をずらすときは、true。金沢の仕様は、true。
