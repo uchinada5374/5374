11fad2f2c32d2ff8a5dbb6eb9f6365baf9ff412f
uchinada5374 <enviro@town.uchinada.lg.jp>
2014-07-02 16:41:44 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -16,7 +16,7 @@ const WeekShift = true; // 休止期間なら週をずらすときは、true。�
 var SVGLabel = false;
 // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var MaxDescription = 9;
+var MaxDescription = 7;
 // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 3;
