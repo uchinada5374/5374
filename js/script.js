e32f0403230421754704f6fde9bbc17a88a998ef
uchinada5374 <enviro@town.uchinada.lg.jp>
2014-10-06 18:51:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -553,7 +553,7 @@ $(function() {
           if (ableSVG && SVGLabel) {
             accordionHTML += '<img src="' + description.styles + '" alt="' + description.label + '"  />';
           } else {
-            accordionHTML += '<p class="text-center">' + description.label + "</p>";
+            accordionHTML += '<p class="text-center">' + "test"  + description.label + "</p>";
           }
           accordionHTML += "</div>" +
             '<h6><p class="text-left date">' + dateLabel + "</p></h6>" +
